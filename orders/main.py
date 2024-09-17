4d560316c5b32ba4756f40297f1d1876a2da3cf1
RiansyahTohamba <mriansyah93@gmail.com>
2024-09-17 15:54:48 +0800

    feat: read json file

--- orders/main.py
+++ orders/main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI, HTTPException
 import httpx
+import json
 
 app = FastAPI()
 
 # Base URL for Sinatra service
 SINATRA_URL = "http://sinatra-service:4567/users"
 
+
 @app.get("/order/{user_id}")
 async def get_order(user_id: int):
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{SINATRA_URL}/{user_id}")
+        # Membaca file order.json
+        order_data = ''
+        with open('order.json', 'r') as file:
+            order_data = json.load(file)
+
         if response.status_code == 200:
             user_data = response.json()
-            return {"order": {"user": user_data, "order_details": "Order details here"}}
+            return {"order": {"user": user_data, "order_details": order_data}}
         else:
             raise HTTPException(status_code=response.status_code, detail="User not found in User Service")
