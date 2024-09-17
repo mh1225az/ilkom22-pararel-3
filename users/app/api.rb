600e8a39ca14a9296459cdd41e3e87564a4d7c6e
RiansyahTohamba <mriansyah93@gmail.com>
2024-09-17 16:41:59 +0800

    feat: update field in handler create user - add migrations

--- users/app/api.rb
+++ users/app/api.rb
@@ -2,25 +2,29 @@ require 'sinatra'
 require 'sequel'
 require 'sqlite3'
 require 'json'
+require 'time'
 
 module UserService
   class API < Sinatra::Base
     # Initialize SQLite database
     DB = Sequel.sqlite("./db/users.db")
 
-    
-
     # Add a user to the database
     post '/users' do
-      data = JSON.parse(request.body.read)
-      name = data['name']
-      email = data['email']
-      res = DB.execute("INSERT INTO users (name, email) VALUES (?, ?)", [name, email])
-      if res
-      else
-      end
+      user_param = JSON.parse(request.body.read)
+      user_param['created_at'] = Time.now
+      user_param['updated_at'] = Time.now
 
-      status 201
+      res = DB[:users].insert(user_param)
+      id = DB[:users].max(:id)
+
+      if res
+        status 201
+        JSON.generate('success'=>true,'user_id' => id,"created_at"=> Time.now)
+      else
+        status 500
+        JSON.generate('success'=>false,'error' => res)
+      end
     end
 
     # Get user by ID
