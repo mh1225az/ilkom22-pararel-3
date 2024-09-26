3a0f998480029aeca9e0a433557d50f92e0cdd26
RiansyahTohamba <mriansyah93@gmail.com>
2024-09-27 06:27:45 +0800

    init dashboard for measure student performance

---
+++ student-performance-dashboard/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudentPerformanceDashboard from './StudentPerformanceDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentPerformanceDashboard />
+    </div>
+  );
+}
+
+export default App;
