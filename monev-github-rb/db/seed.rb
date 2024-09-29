e4f2224a63252b3aeb4c1483d64bcace2c26d5b6
RiansyahTohamba <mriansyah93@gmail.com>
2024-09-29 15:57:12 +0800

    init seed migration

---
+++ monev-github-rb/db/seed.rb
@@ -0,0 +1,22 @@
+require 'csv'
+require 'sqlite3'
+require 'sequel'
+
+DB = Sequel.sqlite("./db/student_dashboard.db")
+csv_file = './data/students.csv'
+# sqlite_db = 'your_sqlite_db.db'
+
+# Open the CSV file
+csv_data = CSV.read(csv_file)
+
+# Extract column names (header row)
+column_names = csv_data.shift
+
+csv_data.each do |row|
+  data_row = {}
+  column_names.zip(row).each do |column_name, value|
+    data_row[column_name] = value
+  end
+  result = DB[:students].insert(data_row)
+  p "successfully insert #{data_row['account_git']}" if result
+end
