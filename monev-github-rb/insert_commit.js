018d3c4046432318dd0e702449e5155afc597c26
RiansyahTohamba <mriansyah93@gmail.com>
2024-09-30 08:33:41 +0800

    revise the code

--- monev-github-rb/insert_commit.js
+++ monev-github-rb/insert_commit.js
@@ -21,11 +21,18 @@ async function fetchCommits() {
     const data = await response.json();
 
     data.forEach(commit => {
+      
       console.log(`Commit: ${commit.sha}`);
       console.log(`Author: ${commit.commit.author.name}`);
       console.log(`Message: ${commit.commit.message}`);
       console.log('--------------------');
     });
+    // nim,name,account_git,commits,lines_added,lines_deleted
+    // F1G122025,Muh. Ilham Arief Prabowo,ilhamariefprabowo,100,1200,0
+
+    // UPDATE students SET commits = commit.count,lines_added = commit.lines_added.count , lines_deleted=commit.lines_deleted.count WHERE account_git= commit.commit.author.name
+
+
   } catch (error) {
     console.error('Error:', error);
   }
