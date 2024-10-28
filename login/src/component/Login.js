94da39af2c42d4b9f215730af2517c0fd5aba9d6
loli hunter <mh1225az@gmail.com>
2024-10-28 21:45:13 +0800

    halaman login

---
+++ login/src/component/Login.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "../component/Login.css"
+
+const Login = () => {
+    return(
+        <div className="awal" style={{marginTop: "200px"}}>
+        <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-md-6">
+                   <div className="card p-4">
+                        <div className="card-body">
+                            <div className="form-group">
+                                <label>Username</label>
+                                <input type="text" placeholder="Username" className="form-control" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input type="password" placeholder="Password" className="form-control" />
+                            </div>
+                        </div>
+                   </div>
+              </div>  
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
