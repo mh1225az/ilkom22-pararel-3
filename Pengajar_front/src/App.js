b77951590d223c834bd4a90b1b77fc13a6e6ae88
loli hunter <mh1225az@gmail.com>
2024-11-19 15:21:55 +0800

    pengajar-sidebar

--- Pengajar_front/src/App.js
+++ Pengajar_front/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Navigationbar from './components/Navigationbar';
+import Body from './components/Card';
 import "./style/LandingPage.css"
+import SideBar from './components/Sidebar';
+
 
 function App() {
   return (
     <div>
       <div  className="myBG">
       <Navigationbar />
-        </div>
+      <SideBar />
+      {/* <Body /> */}
+      </div>
       </div>
     
    
--- Pengajar_front/src/components/Navigationbar.js
+++ Pengajar_front/src/components/Navigationbar.js
@@ -1,22 +1,40 @@
-import { Navbar, Container, Nav } from "react-bootstrap"
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
 const Navigationbar = () => {
-    return(
-      <div className="intro1">
-        <Navbar>
-            <Container>
-                <Navbar.Brand>Online Learning</Navbar.Brand>
-             <Nav>
-                <Nav.Link>HOME</Nav.Link>
-                <Nav.Link>NOTIFICATION</Nav.Link>
-                <Nav.Link>MY COURSE</Nav.Link>
-             </Nav>
-            </Container>
-        </Navbar>
-      </div> 
-            
-        
-    )
-}
+  return (
+    <div className="intro1">
+      <Navbar>
+        <Container>
+          <div>
+            <Navbar.Brand><img src={'http://www.aerosystems.it/wp-content/uploads/2022/01/LMS-logo.png'}
+              alt=""
+              style={{ width: '90px' }}
+            /></Navbar.Brand>
+          </div>
+          <Nav>
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Notivication</Nav.Link>
+            <Nav.Link>My Course</Nav.Link>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Lainnya"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
 
 export default Navigationbar;
